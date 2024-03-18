50ec06fa9551d93ff1778733332646daa2e3da46
arnaldojuniorads@gmail.com <arnaldojuniorads@gmail.com>
2024-03-18 09:49:30 -0300

    Exercicio02

---
+++ Exercicio02.java
@@ -0,0 +1,8 @@
+/*Escreva um algoritmo que leia em um vetor uma sequência finita de números digitados pelo usuário e, logo após, mostre o número de cada posição do vetor e se ele é positivo, negativo ou zero */
+public class Exercicio02 {
+public static void executar(){
+
+    
+
+}   
+}
