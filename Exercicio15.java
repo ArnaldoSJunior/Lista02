9298ae82ec596b80408488c83153c57fcc834a87
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-03-25 09:27:07 -0300

    Program

--- Exercicio15.java
+++ Exercicio15.java
@@ -1,6 +1,7 @@
 /* Elabore um algoritmo que leia 5 números do teclado e preencha um vetor de acordo com a seguinte regra: com exceção do 1º número, só é permitido armazenarum número se ele for maior que o anterior. Ex.: se o primeiro valor lido for 5, o próximo valor lido só poderá ser maior que 5 */
 public class Exercicio15 {
     public static void executar(){
+        
 
     }
 }
